06a8f055e14a6586046af25334114f0a0f6dd8d6
Yeysoon <yeysonaldair13@gmail.com>
2025-12-15 20:22:52 -0600

    Modifications backup db

--- src/db/pool.js
+++ src/db/pool.js
@@ -2,7 +2,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connectionString =
-  'postgresql://admin_siglad:K2wAoSPwxXgPAOflv1oZ9XE3fOcHhdxD@dpg-d3q32hali9vc73c1bj6g-a.oregon-postgres.render.com/siglad_db_909c?sslmode=require';
+  'postgresql://admin_siglad:1BmLmG7No3HmH0kUSbwgy0w58X2GyMTj@dpg-d50b5pfgi27c73agpcr0-a.oregon-postgres.render.com/siglad_db_gj5e';
 
 export const pool = new Pool({
   connectionString,
